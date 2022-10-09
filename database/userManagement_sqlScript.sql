993952997f382efefc7982f7e3cbcd0f07e16f8f
Leon-Salenbacher <leon@salenbacher.com>
2022-10-09 22:27:40 +0200

    c

--- database/userManagement_sqlScript.sql
+++ database/userManagement_sqlScript.sql
@@ -4,6 +4,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+
 -- -----------------------------------------------------
 -- Schema usermanagement
 -- -----------------------------------------------------
@@ -13,6 +14,7 @@ USE `usermanagement` ;
 -- -----------------------------------------------------
 -- Table `usermanagement`.`tblUsers`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `usermanagement`.`tblUsers`;
 CREATE TABLE IF NOT EXISTS `usermanagement`.`tblUsers` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
@@ -25,6 +27,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `usermanagement`.`tblsignInUsers`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `usermanagement`.`tblsignInUsers`;
 CREATE TABLE IF NOT EXISTS `usermanagement`.`tblsignInUsers` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `sessionKey` VARCHAR(45) NOT NULL,
@@ -38,7 +41,6 @@ CREATE TABLE IF NOT EXISTS `usermanagement`.`tblsignInUsers` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
